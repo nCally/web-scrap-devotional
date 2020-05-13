e8e52e5f020938e5938c0dacfdbe5618a3aa7fb7
nCally <cartrf@live.com>
2020-05-14 00:17:45 +0100

    first commit

---
+++ every-day-with-jesus.py
@@ -0,0 +1,65 @@
+import sqlite3
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+
+try:
+
+    conn = sqlite3.connect('db.db')
+    cursor = conn.cursor()
+
+    print('connected')
+
+    devotional_table = '''
+    CREATE TABLE IF NOT EXISTS devotional(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        topic TEXT NOT NULL,
+        reference TEXT,
+        reading TEXT NOT NULL,
+        added DATETIME
+    );
+    '''
+    cursor.execute(devotional_table)  # execute create table
+
+    URL = 'https://www.crosswalk.com/devotionals/everydaylight/'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    content = soup.find_all(
+        'div', class_="articleContentBody devotionalContentBody col-xs-12 col-sm-12 col-md-12")
+
+    for tag in content:
+        div_tag = tag.find_all('div')
+
+        reading = div_tag[0]
+
+        p_tags = tag.find_all('p')
+        main_p_tag = p_tags[1].find_all()
+
+        title = main_p_tag[4]
+        date = main_p_tag[1]
+        meditation = main_p_tag[7]
+
+        todaysReading = (date.text, title.text, meditation.text, reading.text.lstrip(), datetime.datetime.now())
+
+        # adding data into the db
+        add_sql = '''
+            INSERT INTO 
+            devotional(date, topic, reference, reading, added) 
+            VALUES(?,?,?,?,?)
+            '''
+        cursor.execute(add_sql, todaysReading)
+        conn.commit()
+
+        cursor.close()
+
+
+except sqlite3.Error as error:
+    print("Error while creating sqlite table", error)
+
+finally:
+    if(conn):
+        conn.close()
+        print("db connection closed")
