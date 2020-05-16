132c66d9f9e367b4202135fd94c5ac31f8693228
nCally <cartrf@live.com>
2020-05-16 22:40:04 +0100

    will test tomorrow

--- every-day-with-jesus.py
+++ every-day-with-jesus.py
@@ -5,19 +5,19 @@ from bs4 import BeautifulSoup
 
 
 try:
-
     conn = sqlite3.connect('db.db')
     cursor = conn.cursor()
-
-    print('connected')
 
     devotional_table = '''
     CREATE TABLE IF NOT EXISTS devotional(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date TEXT NOT NULL,
+        day TEXT NOT NULL,
+        main TEXT NOT NULL,
+        focus TEXT,
         topic TEXT NOT NULL,
         reference TEXT,
         reading TEXT NOT NULL,
+        prayer TEXT,
         added DATETIME
     );
     '''
@@ -36,51 +36,32 @@ try:
     reading_and_meditation = content1.find('font', class_="headSub")
     related = reading_and_meditation.find_next_siblings()
 
-    main_scripture = related[0].text
-    verse = related[2].text
+    main_scripture = related[0].text # main scriptural reference
+    verse = related[2].text # focus verse
 
     day = strongs[0].text
     title = strongs[1].text
     reading = divs[0].text.lstrip()
     prayer = divs[3].text.lstrip()
 
-    print("References:")
+    other_references = "";
 
     for tags in divs[0].find_next_siblings():
         ass = tags.find_all('a')
         for yas in ass:
-            print(yas.get_text())
+            other_references = other_references + yas.get_text() + ". "
 
-    """ for tag in content:
-        div_tag = tag.find_all('div')
+    todays_reading = (day, main_scripture, verse, title, other_references, reading, prayer, datetime.datetime.now())
 
-        print(div_tag)
-
-        reading = div_tag[0]
-
-        p_tags = tag.find_all('p')
-        main_p_tag = p_tags[1].find_all()
-
-        print(p_tags)
-        print(reading)
-
-        title = main_p_tag[4]
-        date = main_p_tag[1]
-        meditation = main_p_tag[7]
-
-        todaysReading = (date.text, title.text, meditation.text,
-                         reading.text.lstrip(), datetime.datetime.now())
-
-        # adding data into the db
-        add_sql = '''
-            INSERT INTO 
-            devotional(date, topic, reference, reading, added) 
-            VALUES(?,?,?,?,?)
-            '''
-        cursor.execute(add_sql, todaysReading)
-        conn.commit()
-
-        cursor.close() """
+    # adding data into the db
+    add_sql = '''
+        INSERT INTO
+        devotional(day, main, focus, topic, reference, reading, prayer, added)
+        VALUES(?,?,?,?,?,?,?,?)
+        '''
+    cursor.execute(add_sql, todays_reading)
+    conn.commit()
+    cursor.close()
 
 
 except sqlite3.Error as error:
@@ -89,4 +70,4 @@ except sqlite3.Error as error:
 finally:
     if(conn):
         conn.close()
-        print("db connection closed")
+        print("done")
