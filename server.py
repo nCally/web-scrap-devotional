83e727c08f56f34ebca974c7201f39749ae1b562
nCally <cartrf@live.com>
2020-05-19 22:54:00 +0100

    added endpoint

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def example():
-    return '{"name":"Bob"}'
+def get_devotional():
+    conn = sqlite3.connect('db.db')
+    cursor = conn.cursor()
+    cursor.row_factory = sqlite3.Row
+
+    get_query = '''
+    SELECT * FROM devotional;
+    '''
+
+    cursor.execute(get_query)
+    results = cursor.fetchall()
+
+    rowarray_list = []
+    for row in results:
+        d = dict(zip(row.keys(), row))
+        rowarray_list.append(d)
+
+    conn.close()
+
+    return jsonify(rowarray_list)
 
 
 if __name__ == '__main__':
